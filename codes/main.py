10d9a2075fe0e282d2df9fa2d3db8d9768de391a
cjade <garciachristianjade0@gmail.com>
2024-12-22 13:25:40 +0800

    Test using class

--- codes/main.py
+++ codes/main.py
@@ -1,47 +1,42 @@
-class Game:
-    def __init__(self):
-        self.running = False
-        rows = 8
-        self.board = []
-        for _ in range(rows):
-            self.board.append([" " for _ in range(8)])
 
+class Piece:
+    def __init__(self, color="white"):
+        self.color = color
+
+    def validate_move(self):
+        pass
+
+    def move(self):
+        pass
+
+
+class Pawn(Piece):
+    def __init__(self, color="white"):
+        super().__init__(color)
+
+    def __repr__(self):
+        return "P" if self.color == "white" else "p"
+
+class Board:
+    def __init__(self):
+        self.rows = 8
+        self.cols = 8
+        self.board = []
         self.setup_board()
-        
 
 
     def setup_board(self):
-
-        
+        for _ in range(self.rows):
+            self.board.append([" " for _ in range(self.cols)])
 
         self.board[0] = ["r", "n", "b", "q", "k", "b", "n", "r"]
-        self.board[1] = ["p", "p", "p", "p", "p", "p", "p", "p"]
-        self.board[6] = ["P", "P", "P", "P", "P", "P", "P", "P"]
+        self.board[1] = [Pawn("b") for _ in range(self.cols)]
+        self.board[6] = [Pawn() for _ in range(self.cols)]
         self.board[7] = ["R", "N", "B", "Q", "K", "B", "N", "R"]
-
-    def move(self):
-        y_start = int(input("Y Start: "))
-        x_start = int(input("X Start: "))
-        y_end = int(input("Y End: "))
-        X_end = int(input("X End: "))
         
-        self.board[y_end][X_end] = self.board[y_start][x_start] 
-        self.board[y_start][x_start] = " "
-
-        print(self.board[x_start][y_end]) 
 
 
-
-    def draw_board(self):
+    def print_board(self):
         for cell in self.board:
             print(cell)
 
-    def run(self):
-        self.running = True
-        while self.running:
-            self.draw_board()
-            self.move()
-
-
-g = Game()
-g.run()
